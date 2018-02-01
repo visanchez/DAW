3abbc04877ad294eb7fcadc84baaca92df118927
49574026x <49574026x@2A1PC24.ad.fpmislata.com>
2018-02-01 09:58:23 +0100

    Mi primer commit

---
+++ src/Ej1/Comerciales.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ej1;
+
+/**
+ *
+ * @author 49574026X
+ */
+public class Comerciales {
+    private String nombre;
+    private String zona;
+    private String tipoProducto;
+    private String cantidadVendida;
+}
